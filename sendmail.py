bd864216411cc634a1e11b4c6e279bf7f93fd58d
Ahmed Saeed <ahmedsaeedramadan@yahoo.com>
2020-04-21 01:32:31 +0000

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -3,11 +3,11 @@ import email.message
 import re
 
 
-def sendmailg(gserver , gport ,guser, gpass , mailfrom , mailto , msg , ishtml = 0):
+def sendmailg(gserver , gport ,guser, gpass , mailfrom , mailto , subject , msg , ishtml = 0):
     mailmsg = email.message.Message()
     mailmsg['From'] = mailfrom
     mailmsg['To'] = mailto
-    mailmsg['Subject'] = 'Recover Your Account'
+    mailmsg['Subject'] = subject
 
     if ishtml == 1:
         mailmsg.add_header('Content-Type','text/html')
@@ -29,5 +29,6 @@ guser = ''
 gpass = ''
 mailfrom = 'test@test.com'
 mailto = 'test1@test.com,test2@test.com'
+subject = 'test'
 msg = "<html><head></head><body><a href=\"http://www.example.com/\">Please Click the link :)</a></body></html>"
-sendmailg(gserver , gport , guser, gpass , mailfrom , mailto , msg , 1)
+sendmailg(gserver , gport , guser, gpass , mailfrom , mailto , subject , msg , 1)
